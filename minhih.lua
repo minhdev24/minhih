a7fd4358360374bb64f2d1218c09039b69b23661
minhdev24 <minhdev24@gmail.com>
2025-07-17 11:34:01 +0700

    Update minhih.lua

--- minhih.lua
+++ minhih.lua
@@ -1,15 +1,5 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local cmd = require(game.ReplicatedStorage.CmdrClient.Commands.giveseed)
+for i = 1, 12 do cmd() end
 
-local giveSeedEvent = ReplicatedStorage:WaitForChild("CmdrClient"):WaitForChild("Commands"):WaitForChild("giveseed")
-for i = 1, 12 do
-    giveSeedEvent:FireServer("candy blossom")
-end
-
-local givePetEvent = ReplicatedStorage:FindFirstChild("GivePet") or ReplicatedStorage:FindFirstChild("PetEvent")
-if givePetEvent then
-    givePetEvent:FireServer({
-        name = "raccoon",
-        weight = 5,
-        age = 1
-    })
-end
+local pet = require(game.ReplicatedStorage.Modules.PetEggShopController)
+pet.GivePet("Raccoon", 5, 1)
